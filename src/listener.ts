ef67bc1d1fe6e7310222d30f30206e01905c4347
zmmohibul <18-36427-1@student.aiub.edu>
2022-01-12 02:16:03 +0600

    Durable Subscription

--- src/listener.ts
+++ src/listener.ts
@@ -4,22 +4,39 @@ import { randomBytes } from 'crypto';
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-    url: 'http://localhost:4222',
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-    console.log('Listener connected to NATS');
-    
-    const subscription = stan.subscribe('ticket:created', 'order-service-queue-group');
+  console.log('Listener connected to NATS');
 
-    subscription.on('message', (msg: Message) => {
-        console.log('Message Received');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
 
-        const data = msg.getData();
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
 
-        if (typeof data === 'string') {
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-            console.log();
-        }
-    });
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'queue-group-name',
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
